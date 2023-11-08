914a918aff000d94cbf7c97ae7511528c891a765
Ana Rosa <dosanarosa03@gmail.com>
2023-11-08 18:04:39 -0300

    new archive

---
+++ biscoito.py
@@ -0,0 +1,8 @@
+name = "ANNE"
+age = 19
+
+print(f'o nome do usuario é "{name}" e tenho {age} anos')
+
+print(f'o nome do usuario é \"{name}\"\n\te tenho {age} anos')
+
+print(f'o nome do usuario é \'{name}\' e tenho {age} anos')
